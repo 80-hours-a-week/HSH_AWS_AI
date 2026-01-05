fc794cc69f7c11d239b7016a31d12200cc9c4874
ELSAPHABA <sb07907@naver.com>
2026-01-05 15:19:22 +0900

    몬스터 5초 간격 비동기 공격 추가

--- Week2/main.py
+++ Week2/main.py
@@ -1,24 +1,37 @@
-import GameVariableData, MessageGenerator
+from GameVariableData import *
+from MessageGenerator import *
+import asyncio
 
 class MonsterConquest(GameVariableData):
   def __init__(self):
     super().__init__()
     self.msg_gen = MessageGenerator(self)
 
-  def fightprocess(self: int):
+  async def periodic_attack(self):
+    while self.monster_hp > 0:
+        await asyncio.sleep(5)
+        if self.monster_hp > 0:
+            print("\n몬스터가 공격했다! 그러나 아무 일도 없었다.")
+
+  async def fightprocess(self):
     for msg in self.msg_gen.initial_messages():
         print(msg)
+
+    task = asyncio.create_task(self.periodic_attack())
+    loop = asyncio.get_running_loop()
 
     while self.monster_hp > 0:
         for msg in self.msg_gen.turn_start_messages():
             print(msg)
 
-        choice = input(next(self.msg_gen.input_prompt_message()))
+        prompt = next(self.msg_gen.input_prompt_message())
+        choice = await loop.run_in_executor(None, input, prompt)
 
         try:
             skill_name, damage = self.skills[choice]
         except KeyError:
             print("잘못된 입력입니다.")
+            task.cancel()
             exit()
 
         self.monster_hp -= damage
@@ -35,6 +48,9 @@ class MonsterConquest(GameVariableData):
             for msg in self.msg_gen.victory_message():
                 print(msg)
             break
+    
+    task.cancel()
 
-game_instance = MonsterConquest()
-game_instance.fightprocess()
+if __name__ == "__main__":
+    game_instance = MonsterConquest()
+    asyncio.run(game_instance.fightprocess())
